3ed6eb4c0af117840a5b7e0ba10d80b5930c8aaa
NgocNam1512 <ngocnam3518@gmail.com>
2020-12-13 09:12:22 +0700

    update route to private route

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Landing from "../pages/Landing";
+import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Progress from "../pages/Progress";
+import Term from "../pages/Term";
 import Create from "../pages/Create";
 import Search from "../pages/Search";
 import SearchResult from "../pages/SearchResult";
@@ -11,32 +16,43 @@ import Game from "../pages/Game";
 import MainLayout from "../layouts/MainLayout";
 import UserManagerLayout from "../layouts/UserManagerLayout";
 import LearnLayout from "../layouts/LearnLayout";
+import { Container } from "@material-ui/core";
+
 const Routers = () => {
   return (
     <Switch>
       <MainLayout>
         <Switch>
-          <Redirect exact from="/" to="/home" />
-          <Route path="/create" exact component={Create} />
-          <Route path="/search" exact component={Search} />
-          <Route path="/search/result" exact component={SearchResult} />
-          <Route path={["/home"]}>
-            <UserManagerLayout>
-              <Switch>
-                <Route path="/home" exact component={Home} />
-              </Switch>
-            </UserManagerLayout>
-          </Route>
-          <Route path={["/show", "/study", "/exam", "/game"]}>
-            <LearnLayout>
-              <Switch>
-                <Route path="/show" exact component={Show} />
-                <Route path="/study" exact component={Study} />
-                <Route path="/exam" exact component={Exam} />
-                <Route path="/game" exact component={Game} />
-              </Switch>
-            </LearnLayout>
-          </Route>
+          <Route path="/" exact component={Landing} />
+          <Container fixed>
+            <Route path="/login" exact component={Login} />
+            <PrivateRoute path="/create" exact component={Create} />
+            <PrivateRoute path="/search" exact component={Search} />
+            <PrivateRoute
+              path="/search/result"
+              exact
+              component={SearchResult}
+            />
+            <PrivateRoute path="/study" exact component={Study} />
+            <PrivateRoute path="/exam" exact component={Exam} />
+            <PrivateRoute path={["/home", "/progress", "/term"]}>
+              <UserManagerLayout>
+                <Switch>
+                  <PrivateRoute path="/home" exact component={Home} />
+                  <PrivateRoute path="/progress" exact component={Progress} />
+                  <PrivateRoute path="/term" exact component={Term} />
+                </Switch>
+              </UserManagerLayout>
+            </PrivateRoute>
+            <PrivateRoute path={["/show", "/game"]}>
+              <LearnLayout>
+                <Switch>
+                  <PrivateRoute path="/show" exact component={Show} />
+                  <PrivateRoute path="/game" exact component={Game} />
+                </Switch>
+              </LearnLayout>
+            </PrivateRoute>
+          </Container>
         </Switch>
       </MainLayout>
     </Switch>
