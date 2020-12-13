ef933d7eaaabf58d9e12e946facd9462ec7866b9
Hậu Trần <hautran531999@gmail.com>
2020-12-13 22:31:02 +0700

    update

--- src/pages/Exam/index.js
+++ src/pages/Exam/index.js
@@ -85,6 +85,15 @@ const Exam = () => {
     return array;
   };
 
+  const displayTimeLeft = (seconds) => {
+    let minutesLeft = Math.floor(seconds / 60);
+    let secondsLeft = seconds % 60;
+    minutesLeft =
+      minutesLeft.toString().length === 1 ? "0" + minutesLeft : minutesLeft;
+    secondsLeft =
+      secondsLeft.toString().length === 1 ? "0" + secondsLeft : secondsLeft;
+    return `${minutesLeft}:${secondsLeft}`;
+  };
 
   const submit = () => {
     let count = 0;
