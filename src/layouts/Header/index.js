e2a9645ea3e0dfc05e8e3b4a76ca9ec09508c02e
Hậu Trần <hautran531999@gmail.com>
2020-12-13 21:06:53 +0700

    add login,logout

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   AppBar,
@@ -6,20 +6,33 @@ import {
   Typography,
   IconButton,
   Button,
+  Menu,
+  MenuItem,
 } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import NoteAddOutlinedIcon from "@material-ui/icons/NoteAddOutlined";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import { logOutUser } from "../../services/auth";
+import { useAuthentication } from "../../context/auth";
 import "./Header.scss";
 
 const Header = () => {
+  const { currentUser } = useAuthentication();
+  const [openMenu, setOpenMenu] = useState(null);
+  const handleClick = (event) => {
+    setOpenMenu(event.currentTarget);
+  };
+  const handleLogOut = () => {
+    setOpenMenu(null);
+    logOutUser();
+  };
   return (
     <div className="header-container">
       <AppBar position="static" className="header-app-bar">
         <Toolbar>
           <Typography
             component={Link}
-            to="/"
+            to="/home"
             className="header-title"
             variant="h6"
           >
@@ -36,15 +49,29 @@ const Header = () => {
           </Button>
           <div style={{ flexGrow: 0 }} />
           <div>
-            <IconButton
-              className="header-user-icon"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
+            {!currentUser ? (
+              <Button component={Link} to="/login">
+                Đăng Nhập
+              </Button>
+            ) : (
+              <div>
+                <IconButton
+                  className="header-user-icon"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                  onClick={handleClick}
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu keepMounted anchorEl={openMenu} open={Boolean(openMenu)}>
+                  <MenuItem>Profile</MenuItem>
+                  <MenuItem>My account</MenuItem>
+                  <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+                </Menu>
+              </div>
+            )}
           </div>
         </Toolbar>
       </AppBar>
